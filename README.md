93f2fcd2854623c79998e9b84e3ad402dfb9cb6d
burhan <bibrahimi@tricycleltd.com>
2019-01-24 20:32:58 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Srt2Vtt
+=======
+Convert SRT filest to VTT
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist buibr/yii2-srt2vtt "*"
+```
+
+or add
+
+```
+"buibr/yii2-srt2vtt": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \buibr\AutoloadExample::widget(); ?>```
---
+++ src/Srt2Vtt.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace buibr\subtitles;
+
+/**
+ * This is just an example.
+ */
+class Srt2Vtt extends \yii\base\Widget
+{
+    /**
+     *  SRT FILE
+     */
+    private $srt;
+
+    /**
+     * VTT FILE
+     */
+    private $vtt;   // output.
+
+    /**
+     * Handler of fopen
+     */
+    private $handler;
+
+    /**
+     * @param string $srt - path of the srt file as input
+     * @param string $vtt - Vtt gilr 
+     */
+    public function __construct($srt=null, $vtt=null)
+	{
+		$this->srt  = $this->setSrt($srt);
+		$this->vtt  = $this->setVtt($vtt);
+    }
+    
+    /**
+     * @param string $srt
+     */
+    public function setSrt($srt)
+    {
+        if(\is_writable($srt))
+        {
+            $this->srt = $srt;
+        }
+
+        throw new ErrorException("Srt file is not found");
+    }
+
+    /**
+     * @param string $srt
+     */
+    public function setVtt($vtt)
+    {
+        $att = pathinfo($vtt);
+
+        if($att['dirname'] == '.')
+        {
+            $this->vtt = $vtt;
+            return;
+        }
+
+        if(\is_dir($att['dirname']) )
+        {
+            $this->vtt = $vtt;
+        }
+
+        throw new ErrorException("Vtt file is not accessible");
+    }
+
+    /**
+ 	 * Convert srt to vtt
+ 	 *
+ 	 * @return void
+	**/
+	public function convert()
+	{
+		// Read the srt file content into an array of lines
+		$this->handler = fopen($this->srt, 'r');
+
+		if($this->handler) {
+			//	Assume that every line has maximum 8192 length
+			//	If you don't care about line length then you can omit the 8192 param
+			$lines = array();
+
+			//
+			while (($line = fgets($this->handler, 8192)) !== false):
+				$lines[] = $line;
+			endwhile;
+
+            if (!feof($this->handler)) 
+                throw new \ErrorException("Error: unexpected fgets() fail.");
+			else ($this->handler);
+		}
+		// Convert all timestamp lines
+		// The first timestamp line is 1
+		$length = count($lines);
+
+		for ($index = 1; $index < $length; $index++)
+		{
+			// A line is a timestamp line if the second line above it is an empty line
+			if ($index === 1 || trim($lines[$index - 2]) === '')
+			{
+				$lines[$index] = str_replace(',', '.', $lines[$index]);
+			}
+        }
+        
+        /**
+         * 
+         */
+        $content = implode('', $lines);
+        
+        /**
+         * 
+         */
+        $content = utf8_encode($content);
+
+        /**
+         * 
+         */
+		return \file_put_contents($this->vtt, "WEBVTT\n\n" .$content);
+	}
+
+    
+}
